ef571d7663dc419b734d9524d8723f4bdc80fb5d
Semutmanis <aldidermawan1@gmail.com>
2026-03-09 15:40:01 +0700

    mengubah x== menjadi 3000000 dan x+80

--- ControlProgram.cpp
+++ ControlProgram.cpp
@@ -7,10 +7,11 @@ int main(){
   srand(time(0));
   x = rand() % 10;
 
-  if (x==5){
-    x = x+1;
+  if (x==3000000){
+    x = x+80;
     cout << " nilai x sudah di koreksi menjadi : " << x << endl;
   }
 
   cout << " bilangan yang diberikan = " << x;
+
 }
